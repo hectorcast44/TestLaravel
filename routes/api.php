4e6084df0afeea4ce76f51945f8edd908d6907d5
Hector Castro <hectorcastro51680@gmail.com>
2025-10-29 13:05:44 -0600

    Refactor API routing by removing unnecessary comments and directly defining user route with authentication

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EmpleadoApiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-| These routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-*/
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::middleware('api')->group(function () {
-    Route::apiResource('empleados', EmpleadoApiController::class);
-});
+Route::apiResource('empleados', EmpleadoApiController::class);
