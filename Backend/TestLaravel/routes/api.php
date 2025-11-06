f3914009a7bafb1762c2a1696c69044e99c8e28f
Hector Castro <hectorcastro51680@gmail.com>
2025-11-06 11:39:47 -0600

    Implement AuthController with login, register, and logout methods; update API routes accordingly

--- Backend/TestLaravel/routes/api.php
+++ Backend/TestLaravel/routes/api.php
@@ -9,3 +9,7 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('empleados', EmpleadoApiController::class);
+
+Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
